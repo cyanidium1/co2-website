ee146fec0d70f57684811ce4b85d6f8bb9aeed58
AnnaFutryk <anna.futryk@gmail.com>
2024-12-04 12:04:41 +0200

    added benefits

--- axolot/src/components/HomePages/Repair/Services.tsx
+++ axolot/src/components/HomePages/Repair/Services.tsx
@@ -1,22 +1,18 @@
 "use client";
-  
+
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay } from "swiper/modules";
-import Link from "next/link";
+import Image from "next/image";
 
 const Services: React.FC = () => {
   return (
     <>
-      <section id="services" className="repair-services-area ptb-100 pb-0">
+      <section id="services" className="repair-services-area ptb-100 pb-100">
         <div className="container">
           <div className="section-title text-center">
-            <span>Our Services</span>
-            <h3>What we offer for you</h3>
-            <p>
-              We enjoy adapting our strategies to offer every client the best
-              solutions that are at the forefront of the industry.
-            </p>
+            <h3 style={{ color: "#f06334" }}>Переваги</h3>
+            <p>Незалежність у забезпеченні CO2 та навіть більше</p>
           </div>
 
           <Swiper
@@ -41,99 +37,113 @@ const Services: React.FC = () => {
             modules={[Pagination, Autoplay]}
             className="mySwiper repair-services-slides"
           >
-            <SwiperSlide>
+            <SwiperSlide style={{minHeight:'270px'}}>
               <div className="single-repair-services">
-                <i className="icofont-laptop-alt"></i>
-                <h3>Laptop Repair</h3>
+                <Image
+                  src="/icons/chem.svg"
+                  alt="icon"
+                  width={40}
+                  height={40}
+                />
+                <h3>Якість</h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Рідкий CO2 високої якості ≥ 99,998 % обʼємних часток, O2 ≤ 5
+                  ppm обʼємних часток
                 </p>
-                <Link href="#" className="read-more-btn">
-                  Read More
-                </Link>
               </div>
             </SwiperSlide>
 
-            <SwiperSlide>
+            <SwiperSlide style={{minHeight:'270px'}}>
               <div className="single-repair-services">
-                <i className="icofont-computer"></i>
-                <h3>Computer Repair</h3>
+                <Image
+                  src="/icons/progress.svg"
+                  alt="icon"
+                  width={40}
+                  height={40}
+                />
+                <h3>Eфективнiсть відновлення</h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Висока ефективність відновлення навіть при низькій чистоті CO2
+                  на вході з ≥ 95% обʼємних часток.
                 </p>
-                <Link href="#" className="read-more-btn">
-                  Read More
-                </Link>
               </div>
             </SwiperSlide>
 
-            <SwiperSlide>
+            <SwiperSlide style={{minHeight:'270px'}}>
               <div className="single-repair-services">
-                <i className="icofont-responsive"></i>
-                <h3>Apple Products Repair</h3>
+                <Image
+                  src="/icons/manufacturing.svg"
+                  alt="icon"
+                  width={40}
+                  height={40}
+                />
+                <h3>Надійність</h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Міцна конструкція для надійної роботи 24/7, час безвідмовної роботи ≥ 95%
                 </p>
-                <Link href="#" className="read-more-btn">
-                  Read More
-                </Link>
               </div>
             </SwiperSlide>
 
-            <SwiperSlide>
+            <SwiperSlide style={{minHeight:'270px'}}>
               <div className="single-repair-services">
-                <i className="icofont-ui-touch-phone"></i>
-                <h3>Mobile Repair</h3>
+                <Image
+                  src="/icons/extension.svg"
+                  alt="icon"
+                  width={40}
+                  height={40}
+                />
+                <h3>Гнучкість</h3>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                 Гнучка, модульна конструкція для задоволення потреб у плануванні обʼєкта.
                 </p>
-                <Link href="#" className="read-more-btn">
-                  Read More
-                </Link>
+              </div>
+            </SwiperSlide>
+
+             <SwiperSlide style={{minHeight:'270px'}}>
+              <div className="single-repair-services">
+                <Image
+                  src="/icons/eco.svg"
+                  alt="icon"
+                  width={40}
+                  height={40}
+                />
+                <h3>Природні хладагенти</h3>
+                <p>
+                Застосування природних хладагентів CO2 (R744) та NH3 (R717).
+                </p>
+              </div>
+            </SwiperSlide>
+
+             <SwiperSlide style={{minHeight:'270px'}}>
+              <div className="single-repair-services">
+                <Image
+                  src="/icons/light.svg"
+                  alt="icon"
+                  width={40}
+                  height={40}
+                />
+                <h3>Економія з LiquiVap</h3>
+                <p>
+                 Швидке відновлення та економія енергії до 60 % з LiquiVap (опційно).
+                </p>
+              </div>
+            </SwiperSlide>
+
+             <SwiperSlide style={{minHeight:'270px'}}>
+              <div className="single-repair-services">
+                <Image
+                  src="/icons/book.svg"
+                  alt="icon"
+                  width={40}
+                  height={40}
+                />
+                <h3>Підтримка</h3>
+                <p>
+                Доступ до експертних знань про CO2 та технічної підтримки.
+                </p>
               </div>
             </SwiperSlide>
           </Swiper>
-
-          <div className="repair-services-inner">
-            <div className="row">
-              <div className="col-lg-4">
-                <div className="single-inner-services">
-                  <i className="icofont-handshake-deal"></i>
-                  <h3>Quick Repair Process</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor.
-                  </p>
-                </div>
-              </div>
-
-              <div className="col-lg-4">
-                <div className="single-inner-services">
-                  <i className="icofont-wrench"></i>
-                  <h3>Free Diagnostics</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor.
-                  </p>
-                </div>
-              </div>
-
-              <div className="col-lg-4">
-                <div className="single-inner-services">
-                  <i className="icofont-fast-delivery"></i>
-                  <h3>Fast Delivery</h3>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </section>
     </>
